357bf5e0c947318b883d2b76e7967ba7a942077b
atwar917 <atwar917@gmail.com>
2023-05-13 20:13:02 -0400

    new

--- order.php
+++ order.php
@@ -5,8 +5,32 @@ $ccid = $_POST['ccid'];
 
 $total_price = $_POST['total_price'];
 
-$sql = "insert into CustomerOrder_T (cid,ccid,total_price)
-        values ('$cid', '$ccid', '$total_price');
+$query = "SELECT * FROM toppings";
+
+$result = $conn->query($query);
+
+if ($result->num_rows > 0) {
+  // Fetch and store the ingredients in an array
+  $ingredients = [];
+  while ($row = $result->fetch_assoc()) {
+    $ingredients[] = $row;
+  }
+} else {
+  echo "No ingredients found.";
+}
+
+foreach ($ingredients as $ingredient) {
+    echo '<button onclick="handleIngredientClick(' . $ingredient['id'] . ')">' . $ingredient['name'] . '</button>';
+  }
+  
+
+
+
+
+
+
+// $sql = "insert into CustomerOrder_T (cid,ccid,total_price)
+//         values ('$cid', '$ccid', '$total_price');
 
 if ($conn->query($sql)== TRUE) {
     ECHO "Order created successfully";
